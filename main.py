5b1da0e4eb9829d010d6d9a5c0d4e5b89586f8f5
tmby1314 <tmby1314@163.com>
2017-11-17 01:00:46 +0800

    comment

--- main.py
+++ main.py
@@ -5,9 +5,35 @@
 
 """
 """
+# import  socket
+#
+# s = socket.socket()
+# s.connect(('www.baidu.com', 80))
+# print("We are connected to %s:%d" % s.getpeername())
+
+
 import socket
 
-s = socket.socket()
-s.connect(('www.baidu.com', 80))
-print("We are connected to %s:%d" % s.getpeername())
+import time
 
+s = socket.socket()
+s.setblocking(0)
+
+try:
+    s.connect(('www.google.com', 80))
+except socket.error as e:
+    print(str(e))
+    i = 0
+    while True:
+        try:
+            print("We are connected to %s:%d" % s.getpeername())
+            time.sleep(3)
+            break
+        except:
+            print("Let's do some math while waiting: %d" % i)
+            i += 1
+else:
+    print("We are connected to %s:%d" % s.getpeername())
+
+finally:
+    print ("We are connected end")
